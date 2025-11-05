91611117f52b77c98c26ba1722442fff37ce08a1
Tharindu lakmal <161035317+tharindu748@users.noreply.github.com>
2025-11-05 18:07:54 +0530

    databse serviece upgreted

--- backend/src/database/database.service.ts
+++ backend/src/database/database.service.ts
@@ -1,10 +1,23 @@
+// import { Injectable, OnModuleInit } from '@nestjs/common';
+// import { PrismaClient } from '@prisma/client';
+
+// @Injectable()
+// export class DatabaseService extends PrismaClient implements OnModuleInit {
+//   salaryComponent: any;
+//   salarySetup: any;
+//   async onModuleInit() {
+//     await this.$connect();
+//   }
+
+//   async onModuleDestroy() {
+//     await this.$disconnect();
+//   }
+// }
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
-  salaryComponent: any;
-  salarySetup: any;
   async onModuleInit() {
     await this.$connect();
   }
